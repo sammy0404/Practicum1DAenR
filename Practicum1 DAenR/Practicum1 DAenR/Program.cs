921292f514484563e92020f93abfa6b5078b7ba6
Sammie <samvdwal@gmail.com>
2014-04-24 13:03:00 +0200

    SQLite koppeling C#

---
+++ Practicum1 DAenR/Practicum1 DAenR/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SQLite;
+
+namespace Practicum1_DAenR
+{
+    class Program
+    {
+        static SQLiteConnection dbtestobject;
+        static void Main(string[] args)
+        {
+            testdb();
+        }
+
+        static void testdb()
+        {
+            SQLiteConnection.CreateFile("test.sqlite");
+            dbtestobject = new SQLiteConnection("Data Source=test.sqlite; Version=3;");
+            dbtestobject.Open();
+            string invoer1 = ("CREATE TABLE aantalaardigheidspunten (name VARCHAR(20), score INT)");
+            SQLiteCommand opdracht = new SQLiteCommand(invoer1, dbtestobject);
+            opdracht.ExecuteNonQuery();
+            string invoer2 = "insert into aantalaardigheidspunten (name, score) values ('Gerben', 12)";
+            SQLiteCommand command = new SQLiteCommand(invoer2, dbtestobject);
+            command.ExecuteNonQuery();
+            invoer2 = "insert into aantalaardigheidspunten (name, score) values ('Gurbo', 3)";
+            command = new SQLiteCommand(invoer2, dbtestobject);
+            command.ExecuteNonQuery();
+            invoer2 = "insert into aantalaardigheidspunten (name, score) values ('Sammie', 345673)";
+            command = new SQLiteCommand(invoer2, dbtestobject);
+            command.ExecuteNonQuery();
+            string tijdelijkeuitvoer = "select * from aantalaardigheidspunten order by score desc";
+            SQLiteCommand commando = new SQLiteCommand(tijdelijkeuitvoer, dbtestobject);
+            SQLiteDataReader reader = commando.ExecuteReader();
+            while (reader.Read())
+                Console.WriteLine("Name: " + reader["name"] + "\tScore: " + reader["score"]);
+
+
+            dbtestobject.Close();
+            Console.ReadLine();
+
+        }
+    }
+}
